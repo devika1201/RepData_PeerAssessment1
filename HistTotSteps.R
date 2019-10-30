f7d7fe88c4c05dc081b5e2e3b07ac9217da00a6a
devika1201 <50800860+devika1201@users.noreply.github.com>
2019-10-30 11:18:14 +0530

    Delete HistTotSteps.R

--- HistTotSteps.R
+++
@@ -1,4 +0,0 @@
-activity %>% group_by(date) %>% summarise(stepsPerDay = sum(steps)) %>% 
-  ggplot(aes(x = stepsPerDay)) + geom_histogram(bins = 15) + 
-  ggtitle("Histogram: Total Number of Steps Per Day") + xlab("Steps Per Day") + 
-  ylab("Frequency")
