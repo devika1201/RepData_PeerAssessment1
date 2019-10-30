8c751687cb1f84e1988776da10582e3f481709f3
devika1201 <50800860+devika1201@users.noreply.github.com>
2019-10-30 11:18:59 +0530

    Delete ReadProcessData.R

--- ReadProcessData.R
+++
@@ -1,9 +0,0 @@
-activity <- read_csv("activity.csv")
-
-## Parsed with column specification:
-## cols(
-##   steps = col_integer(),
-##   date = col_date(format = ""),
-##   interval = col_integer()
-## )
-
